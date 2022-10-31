34c6616b039951fb828c888947292ac8a4077476
Dan Farrenzo <farrenzo@gmail.com>
2022-10-31 17:57:01 -0400

    Added async methods to it.

--- richprogressbar.py
+++ richprogressbar.py
@@ -41,3 +41,30 @@ def syncronous_run(data_dict:dict) -> None:
                 time.sleep(1)
 
 syncronous_run(data_dict=some_dict) #00:04:35
+
+import asyncio
+async def asynch_advance(progress: Progress, task_id: int) -> None:
+    while progress._tasks[task_id].completed != progress._tasks[task_id].total:
+        progress.update(task_id, advance=1)
+        await asyncio.sleep(1)
+
+async def asynchronous_run(dict_to_run: dict) -> None:
+    console = Console(record=False)
+    with Progress(
+        SpinnerColumn(),
+        *Progress.get_default_columns(),
+        TimeElapsedColumn(),
+        console=console,
+    ) as progress:
+        tasks = {
+            key:progress.add_task(f"[green]Running [b]{key}[/b]", total=value)
+                for key, value in dict_to_run.items()
+        }
+        await asyncio.gather(*(
+            asynch_advance(progress, task_id) for task_name, task_id in tasks.items()
+        ))
+    
+start = time.perf_counter()
+asyncio.run(asynchronous_run(dict_to_run=dict_165))
+finish = time.perf_counter()
+print(f"Done in {finish - start} seconds.")
